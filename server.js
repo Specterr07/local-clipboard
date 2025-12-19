38edce4d4952fdad7f745eaa5e444143ffdadfb2
Specterr07 <vivek74203@gmail.com>
2025-12-20 00:11:19 +0530

    add views and update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+
+app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
-    res.send("System Online");
-})
+    res.render('index', {title: "Local Clipboard",text: "Waiting for data", files: []});
+});
 
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
