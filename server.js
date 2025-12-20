0a0cbdd36778ea8f67a96ff848e77d844898e23f
Specterr07 <vivek74203@gmail.com>
2025-12-20 13:37:51 +0530

    update server to handle post and add a todo.md file

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+let sharedData = "No text shared yet";
 
+app.use (express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+app.post('/update-text', (req, res) => {
+    const recentText = req.body.content;
+    sharedData = recentText;
+    console.log(`Text Copied: ${sharedData}`)
+    res.redirect('/')
+})
+
 app.get('/', function (req, res) {
     res.render('index', {title: "Local Clipboard",text: "Waiting for data", files: []});
 });
---
+++ todo.md
@@ -0,0 +1,6 @@
+# Todo
+
+## Features to Add.
+
+1. Drag and File Area.
+2. The mobile web app should directly copy things to clipboard on refreshing the webApp(instead of again copying).
