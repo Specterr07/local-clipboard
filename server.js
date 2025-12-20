25ac9222d5976c5ec3bc6b757179ceb04352cee9
Specterr07 <vivek74203@gmail.com>
2025-12-20 20:01:13 +0530

    modified by making the output dynamic

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ app.set('view engine', 'ejs');
 
 app.post('/update-text', (req, res) => {
     const recentText = req.body.content;
+    const body = req.body;
     sharedData = recentText;
     console.log(`Text Copied: ${sharedData}`)
+
+    console.log(`Copied Object: ${body}`) // console.log wont log the object's content.
+    console.table(body)// console.table is able to log content of the object.
     res.redirect('/')
+
 })
 
 app.get('/', function (req, res) {
-    res.render('index', {title: "Local Clipboard",text: "Waiting for data", files: []});
+    res.render('index', {title: "Local Clipboard",text: sharedData, files: []});
 });
 
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
